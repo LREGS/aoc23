9a8435f10646cd538c212fdb1dff04db958e4100
William Cullum <william-cullum@hotmail.co.uk>
2023-12-17 14:08:42 +0000

    still not working :(

--- d1.py
+++ d1.py
@@ -1,9 +1,6 @@
 import re
-"""I think we can take each string and seperate it into str/ints and then 
-just use the first and last element if len > 1 
-"""
 
-"""Could you have just gone through each string and took the first and last int with regex?"""
+
 with open('d1_data.txt') as file:
     data = file.read()
     allCodes = data.split('\n')
@@ -26,31 +23,58 @@ d ={
 seperatedCodes = [code for code in allCodes]
 
 values = [code for code in seperatedCodes]
+
 vals =[]
 for code in values:
     nums = re.findall("one|1|two|2|three|3|four|4|five|5|six|6|seven|7|eight|8|nine|9", code)
     vals.append(nums)
+"""maybe use slice and not regex - maybe like a for loop going over each char, and splitting it each time a
+and adding it into a list - and then checking that list each iteration to see if the word has appeared or not 
+so it would be lke o on one oner etc and you would get true for one. 
 
+Still not sure if this fixes the oneight sitation though. 
 
-huh = []
-for l in vals:
-    for v in l:
-        if v in d.keys():
-            target = l.index(v)
-            # print(l[target],v, target,d[v])
-            l[target] = d[v]   
+Basically slice the string from the begining until a word or int it found, then once either a matching expression is found
+or after an int is found then search the back of the string"""
 
-total = 0
-for val in vals:
+# for val in values:
+#     for i in range(len(val)):
+#         first = slice(0,i)
+#         print(val[first])
+#         if val[first].isdigit():
+#             print(val[first])
+#             break
+#         if val[first] in d.keys():
+#             print(val[first])
+#             break
+#         else:
+#             print('nothing')
 
-    if len(val) <= 1:
-        code = (str(val[0]) + str(val[0]))
-        total = total + int(code)
+for val in values:
+    for key in d.keys():
+        if key in val:
+            print(val.index(key), key)
 
-    else:
-        code = (str(val[0]) + str(val[-1]))
-        total = total + int(code)
-print(total)
+#maybe I need to slice at the number - so does the string before the number contain the string of one of the keys?
+#or does the string after the number contain letters - but then what if there is two strings before?
+
+# for l in vals:
+#     for v in l:
+#         if v in d.keys():
+#             target = l.index(v)
+#             # print(l[target],v, target,d[v])
+#             l[target] = d[v]   
+
+# total = 0
+# for val in vals:
+
+#     if len(val) <= 1:
+#         code = (str(val[0]) + str(val[0]))
+#         total = total + int(code)
+
+#     else:
+#         code = (str(val[0]) + str(val[-1]))
+#         total = total + int(code)
     
 #     if len(val) <= 1:
 #         code = (str(val[0]) + str(val[0]))
---
+++ t.py
@@ -0,0 +1,5 @@
+txt = "1k1k1k"
+
+for char in txt:
+    if char.isdigit():
+        print(char)
